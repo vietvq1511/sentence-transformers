6cfe4bb9548835a1dec47354931a8064c8bfd440
Nguyen Van Trua <15520934@gm.uit.edu.vn>
2020-05-14 17:59:03 +0700

    add training NLI file

--- examples/training_transformers/training_nli.py
+++ examples/training_transformers/training_nli.py
@@ -2,10 +2,8 @@
 The system trains BERT (or any other transformer model like RoBERTa, DistilBERT etc.) on the SNLI + MultiNLI (AllNLI) dataset
 with softmax loss function. At every 1000 training steps, the model is evaluated on the
 STS benchmark dataset
-
 Usage:
 python training_nli.py
-
 OR
 python training_nli.py pretrained_transformer_model_name
 """
@@ -13,11 +11,24 @@ from torch.utils.data import DataLoader
 import math
 from sentence_transformers import models, losses
 from sentence_transformers import SentencesDataset, LoggingHandler, SentenceTransformer
-from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
+from sentence_transformers.evaluation import LabelAccuracyEvaluator
 from sentence_transformers.readers import *
 import logging
 from datetime import datetime
 import sys
+import os
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--batch_size', type=int, default=24)
+parser.add_argument('--evaluation_steps', type=int, default= 1000)
+parser.add_argument('--ckpt_path', type=str, "./output")
+parser.add_argument('--num_epochs', type=int, "1")
+parser.add_argument('--data_path', type=str, "./DataNLI")
+parser.add_argument('--pre_trained_path', type=str, "./PhoBert")
+args = parser.parse_args()
+
 
 #### Just some code to print debug information to stdout
 logging.basicConfig(format='%(asctime)s - %(message)s',
@@ -26,19 +37,20 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
                     handlers=[LoggingHandler()])
 #### /print debug information to stdout
 
+if not os.path.exists(args.ckpt_path):
+    os.mkdir(args.ckpt_path)
+
 #You can specify any huggingface/transformers pre-trained model here, for example, bert-base-uncased, roberta-base, xlm-roberta-base
 model_name = sys.argv[1] if len(sys.argv) > 1 else 'bert-base-uncased'
 
 # Read the dataset
-batch_size = 16
-nli_reader = NLIDataReader('../datasets/AllNLI')
-sts_reader = STSBenchmarkDataReader('../datasets/stsbenchmark')
+args.batch_size = 16
+nli_reader = NLIDataReader(args.data_path)
 train_num_labels = nli_reader.get_num_labels()
-model_save_path = 'output/training_nli_'+model_name.replace("/", "-")+'-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 
 # Use Huggingface/transformers model (like BERT, RoBERTa, XLNet, XLM-R) for mapping tokens to embeddings
-word_embedding_model = models.Transformer(model_name)
+word_embedding_model = models.PhoBERT.load(args.pre_trained_path)
 
 # Apply mean pooling to get one fixed sized sentence vector
 pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(),
@@ -50,22 +62,20 @@ model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
 
 
 # Convert the dataset to a DataLoader ready for training
-logging.info("Read AllNLI train dataset")
+logging.info("Read XNLI train dataset")
 train_data = SentencesDataset(nli_reader.get_examples('train.gz'), model=model)
-train_dataloader = DataLoader(train_data, shuffle=True, batch_size=batch_size)
+train_dataloader = DataLoader(train_data, shuffle=True, batch_size=args.batch_size)
 train_loss = losses.SoftmaxLoss(model=model, sentence_embedding_dimension=model.get_sentence_embedding_dimension(), num_labels=train_num_labels)
 
+#convert the dataset to a Dataloader ready for dev
+logging.info("Read XNLI dev dataset")
 
-
-logging.info("Read STSbenchmark dev dataset")
-dev_data = SentencesDataset(examples=sts_reader.get_examples('sts-dev.csv'), model=model)
-dev_dataloader = DataLoader(dev_data, shuffle=False, batch_size=batch_size)
-evaluator = EmbeddingSimilarityEvaluator(dev_dataloader)
+dev_data = SentencesDataset(nli_reader.get_examples('dev.gz'), model=model)
+dev_dataloader = DataLoader(dev_data, shuffle=False, batch_size=args.batch_size)
+evaluator = LabelAccuracyEvaluator(dev_dataloader)
 
 # Configure the training
-num_epochs = 1
-
-warmup_steps = math.ceil(len(train_dataloader) * num_epochs / batch_size * 0.1) #10% of train data for warm-up
+warmup_steps = math.ceil(len(train_dataloader) * args.num_epochs / args.batch_size * 0.1) #10% of train data for warm-up
 logging.info("Warmup-steps: {}".format(warmup_steps))
 
 
@@ -73,23 +83,24 @@ logging.info("Warmup-steps: {}".format(warmup_steps))
 # Train the model
 model.fit(train_objectives=[(train_dataloader, train_loss)],
           evaluator=evaluator,
-          epochs=num_epochs,
-          evaluation_steps=1000,
+          epochs=args.num_epochs,
+          evaluation_steps=args.evaluation_steps,
           warmup_steps=warmup_steps,
-          output_path=model_save_path
+          output_path=args.ckpt_path
           )
 
 
 
 ##############################################################################
 #
-# Load the stored model and evaluate its performance on STS benchmark dataset
+# Load the stored model and evaluate its performance on XNLI dataset
 #
 ##############################################################################
 
 model = SentenceTransformer(model_save_path)
-test_data = SentencesDataset(examples=sts_reader.get_examples("sts-test.csv"), model=model)
-test_dataloader = DataLoader(test_data, shuffle=False, batch_size=batch_size)
-evaluator = EmbeddingSimilarityEvaluator(test_dataloader)
+
+test_data = SentencesDataset(nli_reader.get_examples('test.gz'), model=model)
+test_dataloader = DataLoader(test_data, shuffle=False, batch_size=args.batch_size)
+evaluator = LabelAccuracyEvaluator(test_dataloader)
 
 model.evaluate(evaluator)
